3cc968d3d663eccc2c7025a2a9f99f263a7022c5
Raygud <78358597+Raygud@users.noreply.github.com>
2021-10-25 15:33:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let namer = "Rùni Gudmundarson"
+let str = "<h1>" + namer + "<h1/>".innerHTML
+
+function nameload(){
+    document.getElementById("namer").innerHTML = "&lt;h1&gt;" + namer + "&lt;h1/&gt;";
+
+
+let str = "HELLO WORLD";
+document.getElementById("namer").innerHTML = str.charAt(4);}
