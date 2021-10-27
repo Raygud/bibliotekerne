bbea574770a4c72a9f4e283f49aeb2fefd67fe9c
Raygud <runigudmundarson@hotmail.com>
2021-10-27 20:36:18 +0200

    k

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const DayOfTheWeek = [
+    "Mandag",
+    "Tirsdag",
+    "Onsdag",
+    "Torsdag",
+    "Fredag",
+    "Lørdag",
+    "Søndag"
+];
 
+const d = new Date();
+let day = d.getDay()
+DayOfTheWeekWrite = DayOfTheWeek[day-1]
+document.getElementById("CurrentDayTime").innerHTML = "Åbner om:";
